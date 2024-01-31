37692683bfdc7d546fc0c26e5249f94b02847d54
Alex Chubaty <achubaty@for-cast.ca>
2024-01-31 13:03:35 -0700

    need Require

--- R/packageTools.R
+++ R/packageTools.R
@@ -234,7 +234,7 @@ updateGit <- function(pkgs = NULL,
 
 .pkgDepsGraph <- function(pkgs) {
   dt <- lapply(pkgs, function(pkg) {
-    deps1 <- tryCatch(reproducible::pkgDep(pkg), error = function(x) "")
+    deps1 <- tryCatch(Require::pkgDep(pkg), error = function(x) "")
     deps <- pkgs[pkgs %in% deps1[[1]]]
     if (NROW(deps))
       data.table::data.table(pkg = pkg,
